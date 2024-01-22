2a06c466c37ce8ac107e5b4f08da9a28931e2bf3
Khoi Vu <kimkhoivu@gmail.com>
2024-01-22 17:28:25 +0700

    Add repositories for role, customer

---
+++ src/main/java/com/JavaBootcamp03/mobilestore/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.JavaBootcamp03.mobilestore.repository;
+
+import com.JavaBootcamp03.mobilestore.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<CustomerEntity, Integer> {
+    CustomerEntity findByEmail(String email);
+}
