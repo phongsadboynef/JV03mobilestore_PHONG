d0ccb4760a6fa83994690488625711d49c30a679
Khoi Vu <kimkhoivu@gmail.com>
2024-01-22 11:19:59 +0700

    Update Springboot version, Model Mapper dependency

--- src/main/java/com/JavaBootcamp03/mobilestore/MobilestoreApplication.java
+++ src/main/java/com/JavaBootcamp03/mobilestore/MobilestoreApplication.java
@@ -1,11 +1,17 @@
 package com.JavaBootcamp03.mobilestore;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MobilestoreApplication {
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MobilestoreApplication.class, args);
 	}
