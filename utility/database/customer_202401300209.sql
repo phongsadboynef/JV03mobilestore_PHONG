82a8b9739c88f0e6dda463b7f7fce6af55a65056
Khoi Vu <kimkhoivu@gmail.com>
2024-01-30 02:11:54 +0700

    Data script for customer

---
+++ utility/database/customer_202401300209.sql
@@ -0,0 +1,16 @@
+INSERT INTO mobileStore.customer (email,password,custFirstName,custLastName,custDOB,custGender,custRank,id_role,createdAt,deletedAt) VALUES
+	 ('customer123','$2a$10$WKcPPGBXatFjphrUnAx8tuaSm27FKtOYNGwS7y.wm9n61t9LiOe7C','Customer','The First','2000-03-29',0,'Vàng',3,'2024-01-20 17:57:26',NULL),
+	 ('hoangdinhphu@hotmail.com','$2a$10$BETnf4K5QrqaAzaw/VRNV.5cDLICWuisYAtJLMI2NiZVeuixtbfLm','Phú','Hoàng Đình','1982-09-11',1,'Đồng',3,'2024-01-20 17:57:26',NULL),
+	 ('nguyenthingocngan@yahoo.com','$2a$10$.meGN/.aCQSyGAE1Z6SSHuAUQQRoPXlAJKtOwW3lYcx/qzirPJUG6','Ngân','Nguyễn Thị Ngọc','1991-03-12',0,'Bạc',3,'2024-01-20 17:57:26',NULL),
+	 ('chungvanhuy@specialmail.com','$2a$10$2YXzuD0cmTT33eY.gNytOe.dYwLwDz2fNQAunApTWPSPeR4.ZG7xC','Huy','Chung Văn','1993-07-16',1,'Đồng',3,'2024-01-20 17:57:26',NULL),
+	 ('luongmyphuong@somemail.com','$2a$10$5uR4EmL2xCZormtgD0gHi.ppvdasajkYc.BuT1NX5H0Oj4ztr/m/S','Phương','Lương Mỹ','1999-02-18',0,'Bạch Kim',3,'2024-01-20 17:57:26',NULL),
+	 ('sonvu@gmail.com','$2a$10$YPQHZ3gL7.1ngyufYrm2ZemHDVjue5vMhsb4Aza6RgUq8ulut3PkG','Sơn','Vũ','1997-08-15',1,NULL,3,NULL,NULL),
+	 ('kimkhoiv@gmail.com','$2a$10$twNf3d49ZpCfc5rUuVMs1OSP93V80XmN1on/Gq2AcFCEPZ00s8t/.','Khôi','Vũ','1987-10-27',1,NULL,3,NULL,NULL),
+	 ('helenngo@gmail.com','$2a$10$cm1qDJNNpMctUqv2vpTkk.K2oMJ2qj.cC0uKBs78r83j0RabNjRP2','Helen','Ngô','2001-02-12',0,NULL,3,NULL,NULL),
+	 ('foxiewu@gmail.com','$2a$10$/DP26wUDpOcv/n5l6I6B5OEfAzpfk85ijMtxFqTxQ3XcY9uZhDf5u','Foxie','Wu','1989-10-26',0,NULL,3,NULL,NULL),
+	 ('jackiema@sparrowmail.com','$2a$10$ICSxbzjuFa4q9XVLaCXSx.KQ2Ea/I4XoVVdEeuMHBEqvC.Og9csLS','Jackie','Ma','2002-01-19',1,NULL,2,NULL,NULL);
+INSERT INTO mobileStore.customer (email,password,custFirstName,custLastName,custDOB,custGender,custRank,id_role,createdAt,deletedAt) VALUES
+	 ('alextrinh@hotmail.com','$2a$10$sob4kAB/6iKnv1zjjavrO.Lv8Tai.qB3nFrgWAPpvQ5zOT8IHe9Wy','Alex','Trinh','2003-03-23',1,NULL,2,NULL,NULL),
+	 ('hoangphihong@gmail.com','$2a$10$L6VaJntURe3ARqFru1nPTesr83iatxlbSF3WukKJdsKYS2Mx5.ZCe','Hồng','Hoàng Phi','2003-04-26',1,NULL,3,NULL,NULL),
+	 ('jorgehuntington123@athletes.com','$2a$10$DVSW4idUptQfsiCUqFzEL.t.Hxqy.woyw1LL66joqms/elZqu.wWi','Jorge','Huntington','1990-11-02',1,NULL,3,NULL,NULL),
+	 ('thanhtran456','$2a$10$I4Et5Wf9yHPr.U3k36be7Ob7mWpxyNHikOJ6kqXziDPvLN.FxKeYC','Thiên Thanh','Trần Thị','1989-07-28',0,NULL,3,NULL,NULL);
