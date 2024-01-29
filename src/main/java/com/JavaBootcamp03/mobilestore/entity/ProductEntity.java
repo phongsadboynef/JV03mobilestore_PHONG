ab1bd4cb56b7117af78b2be2f6c10432f891b8cb
Khoi Vu <kimkhoivu@gmail.com>
2024-01-29 15:04:11 +0700

    Update Product Entity, DTO, RelatedProductController

--- src/main/java/com/JavaBootcamp03/mobilestore/entity/ProductEntity.java
+++ src/main/java/com/JavaBootcamp03/mobilestore/entity/ProductEntity.java
@@ -2,9 +2,14 @@ package com.JavaBootcamp03.mobilestore.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
 @Data
+@Getter
+@Setter
 @Entity(name = "product")
 public class ProductEntity {
     @Id
@@ -17,7 +22,13 @@ public class ProductEntity {
     @Column(name = "title")
     private String title;
 
+    @Column(name = "createdAt")
+    private String createdAt;
+
+    @Column(name = "deletedAt")
+    private String deletedAt;
+
     @ManyToOne
     @JoinColumn(name = "id_subCat")
-    private SubCategoryEntity subCategory;
+    private SubCategoryEntity productSubCat;
 }
