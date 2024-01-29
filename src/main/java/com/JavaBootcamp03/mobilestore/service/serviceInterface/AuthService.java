c4b64eb8fb5b3baa49e67342a04cf68726bf53e9
Khoi Vu <kimkhoivu@gmail.com>
2024-01-30 01:46:58 +0700

    Apply Jwt Authentication

--- src/main/java/com/JavaBootcamp03/mobilestore/service/serviceInterface/AuthService.java
+++
@@ -1,7 +0,0 @@
-//package com.JavaBootcamp03.mobilestore.service.serviceInterface;
-//
-//import com.JavaBootcamp03.mobilestore.dto.CustomerDTO;
-//
-//public interface AuthService {
-//    String login(CustomerDTO customerDto);
-//}
