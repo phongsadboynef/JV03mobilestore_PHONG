eaa2c45446bdc4fc3575d24b7d0e680f8f31e0cf
Khoi Vu <kimkhoivu@gmail.com>
2024-01-29 19:44:37 +0700

    Data scripts for category, subCategory, product, review

---
+++ utility/database/category_202401271420.sql
@@ -0,0 +1,3 @@
+INSERT INTO mobileStore.category (catName,catDescription,createdAt,deletedAt) VALUES
+	 ('Mobile','Mobile phone products',NULL,NULL),
+	 ('Laptop','Laptop products',NULL,NULL);
---
+++ utility/database/subCategory_202401271421.sql
@@ -0,0 +1,4 @@
+INSERT INTO mobileStore.subCategory (subCatName,subCatDescription,subCat_parentID,id_category,createdAt,deletedAt) VALUES
+	 ('iPhone','iPhone mobile phone',0,1,NULL,NULL),
+	 ('Samsung','Samsung mobile phone',0,1,NULL,NULL),
+	 ('Dell','Dell laptop',0,2,NULL,NULL);
